e5ebf8b56a8bbe0e7ffae3a22ebf60bef2a5bc11
Fedriek Chandra <fedriekchandra@gmail.com>
2021-01-16 14:21:17 +0700

    add sanctum and vuejs

---
+++ resources/views/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Word Scramble</title>
+</head>
+<body>
+
+    <div id="app">
+        <router-view></router-view>
+    </div>
+
+<script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
